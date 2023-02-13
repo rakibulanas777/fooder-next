2b00be1370485a5db12ac510e97b618c27b25e55
unknown <rakibulanas033@gmail.com>
2023-02-13 22:49:40 +0600

    ad

--- src/component/FoodBanner.js
+++ src/component/FoodBanner.js
@@ -3,7 +3,6 @@ import Link from "next/link";
 import React from "react";
 import { AiOutlineArrowUp, AiOutlinePlus } from "react-icons/ai";
 import styled from "styled-components";
-import data from "./food";
 
 const FoodBanner = ({ foods, handleCatagory }) => {
 	return (
